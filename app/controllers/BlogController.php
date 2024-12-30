0ac793ebe95afbc711502975edfbfb7f4aa0a557
Giovanne Monteiro <101524886+Gmmbr10@users.noreply.github.com>
2024-12-30 13:14:57 -0300

    create "create post" page

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -45,4 +45,23 @@ class BlogController
 		
 	}
 
+	public function create(array $data)
+	{
+		$links = file_get_contents(__DIR__ . "/../views/components/style.html");
+		$header = file_get_contents(__DIR__ . "/../views/components/header-default.html");
+		$footer = file_get_contents(__DIR__ . "/../views/components/footer.html");
+		$html = file_get_contents(__DIR__ . "/../views/pages/createPost.html");
+
+		$header = str_replace( "{inicio}" , "" , $header );
+		$header = str_replace( "{blog}" , "" , $header );
+		$header = str_replace( "{about}" , "" , $header );
+
+		$html = str_replace( "{script_style}" , $links , $html );
+		$html = str_replace( "{component_header}" , $header , $html );
+		$html = str_replace( "{component_footer}" , $footer , $html );
+		$html = str_replace( "{include_path}" , INCLUDE_PATH , $html );
+
+		echo $html;
+	}
+
 }
