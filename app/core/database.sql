ee762ce32e66446ded595419bd214b5d654a4bad
Giovanne Monteiro <101524886+Gmmbr10@users.noreply.github.com>
2025-01-09 17:01:21 -0300

    atualizando o script do banco de dados e adicionando tabela de posts e reviews

--- app/core/database.sql
+++ app/core/database.sql
@@ -7,5 +7,26 @@ create table if not exists users (
   user_name varchar(150) not null,
   user_email varchar(255) not null,
   user_password varchar(255) not null,
+  user_active tinyint not null default(1),
   primary key(user_id)
+);
+
+create table if not exists posts (
+  post_id int auto_increment not null,
+  post_title varchar(255) not null,
+  post_content text not null,
+  post_userId int not null,
+  post_countView int not null default(0),
+  post_active tinyint not null default(1),
+  primary key(post_id),
+  foreign key(post_userId) references users(user_id) on delete cascade
+);
+
+create table if not exists reviews (
+  review_id int auto_increment not null,
+  review_postId int not null,
+  review_auth tinyint not null default(0),
+  review_message text not null,
+  primary key(review_id),
+  foreign key(review_postId) references posts(post_id) on delete cascade
 );
